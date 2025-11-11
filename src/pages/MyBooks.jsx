f6b2b6e0fb9218678554edb47e86999fd37abd4a
Najmulkhan1 <najmulislam624@gmail.com>
2025-11-11 18:01:03 +0600

    add delete and update functionality

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -7,11 +7,11 @@ import { FaRegEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Swal from "sweetalert2";
 
-
 const MyBooks = () => {
   const { user } = useAuth();
   const axiosInstance = useAxios();
   const [books, setBooks] = useState([]);
+  const [edit, setEdit] = useState(null);
   const editBookRef = useRef();
 
   useEffect(() => {
@@ -60,16 +60,78 @@ const MyBooks = () => {
   };
 
   const handleEdit = (id) => {
+    const findBook = books.find((b) => b._id === id);
+    if (!findBook) {
+      console.log("book not found", id);
+      return;
+    }
+
+    setEdit({ ...findBook });
+
     editBookRef.current.showModal();
     console.log(id);
   };
 
-  const handleFormSubmit = (e,id) => {
-    e.preventdefault()
 
-    console.log(id);
-    
-  }
+  const handleFormSubmit = async (e) => {
+     e.preventDefault();
+
+    const id = edit?._id
+
+    const form = e.target;
+    const title = form.title.value;
+    const author = form.author.value;
+    const authorImg = form.authorImg.value;
+    const category = form.category.value;
+    const rating = form.rating?.value;
+    const summary = form.summary.value;
+    const bookImage = form.bookImage.value;
+    const userEmail = form.email.value;
+    const userName = form.name.value;
+
+    const newBook = {
+        title: title,
+        author: author,
+        authorImg: authorImg,
+        category: category,
+        rating: parseFloat(rating),
+        summary: summary,
+        bookImage: bookImage,
+        userEmail: userEmail,
+        userName: userName,
+    }
+
+    try {
+        await axiosInstance.put(`/books/${id}`,newBook)
+        // const updated = res.data;
+
+        setBooks(prev =>
+  prev.map(b => (b._id === id ? { ...b, ...newBook } : b))
+);
+
+        Swal.fire({
+        title: "Updated!",
+        text: "Book updated successfully.",
+        icon: "success",
+      });
+
+    if (editBookRef.current) editBookRef.current.close();
+ 
+        
+    } catch (error) {
+         console.error("Update failed:", error);
+      Swal.fire({
+        title: "Error",
+        text: "Could not update the book. Try again.",
+        icon: "error",
+      });
+    }
+  };
+
+
+
+
+
 
   return (
     <div>
@@ -126,7 +188,7 @@ const MyBooks = () => {
             <tbody className=" divide-y divide-gray-100">
               {books.map((book) => (
                 <tr
-                  key={book.id}
+                  key={book._id}
                   className="hover:bg-gray-50 transition-colors"
                 >
                   <td className="px-4 py-4 flex items-center gap-4">
@@ -166,7 +228,9 @@ const MyBooks = () => {
                       </button>
 
                       <button
-                        onClick={() =>{handleEdit(book._id)}}
+                        onClick={() => {
+                          handleEdit(book._id);
+                        }}
                         className="inline-flex items-center gap-2 px-3 py-2 rounded-lg border bg-white hover:bg-gray-50 text-sm"
                       >
                         <FaRegEdit />
@@ -174,160 +238,206 @@ const MyBooks = () => {
 
                       <dialog
                         ref={editBookRef}
+                        
                         className="modal modal-bottom sm:modal-middle"
                       >
                         <div className="modal-box">
-                         <form onSubmit={() => handleFormSubmit(book._id)} className="space-y-3">
-            <div className="grid grid-cols-2 gap-4">
-              {/* title field */}
-              <div>
-                <label className="block">Title</label>
-                <input
-                  type="text"
-                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  name="title"
-                  required
-                />
-              </div>
+                          <form
+                           key={edit?._id ?? "empty-edit"} 
+                            onSubmit={handleFormSubmit}
+                            className="space-y-3"
+                          >
+                            <div className="grid grid-cols-2 gap-4">
+                              {/* title field */}
+                              <div>
+                                <label className="block">Title</label>
+                                <input
+                                  type="text"
+                                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  name="title"
+                                  defaultValue={edit?.title}
+                                  required
+                                />
+                              </div>
 
-              {/* author Field */}
-              <div>
-                <label className="block">Author</label>
-                <input
-                  type="text"
-                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  name="author"
-                  required
-                />
-              </div>
-            </div>
+                              {/* author Field */}
+                              <div>
+                                <label className="block">Author</label>
+                                <input
+                                  type="text"
+                                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  name="author"
+                                  required
+                                   defaultValue={edit?.author}
+                                />
+                              </div>
+                            </div>
 
-            {/* author image */}
-            <div>
-              <label className="block">
-                Author Image Url{" "}
-                <span className="text-gray-500">(optional)</span>
-              </label>
-              <input
-                type="url"
-                className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                name="authorImg"
-              />
-            </div>
+                            {/* author image */}
+                            <div>
+                              <label className="block">
+                                Author Image Url{" "}
+                                <span className="text-gray-500">
+                                  (optional)
+                                </span>
+                              </label>
+                              <input
+                                type="url"
+                                className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                name="authorImg"
+                               
+                              />
+                            </div>
 
-            {/* genre category */}
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <label className="block">Genre</label>
-                <select
-                  name="category"
-                  className="select w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  defaultValue='1'
-                  required
-                >
-                  <option value="1" disabled>
-                    -- Select Genre --
-                  </option>
-                  <option value="Classic Fiction">Classic Fiction</option>
-                  <option value="Historical Fiction">Historical Fiction</option>
-                  <option value="Fantasy">Fantasy</option>
-                  <option value="Science Fiction">Science Fiction</option>
-                  <option value="Mystery">Mystery</option>
-                  <option value="Thriller">Thriller</option>
-                  <option value="Romance">Romance</option>
-                  <option value="Adventure">Adventure</option>
-                  <option value="Non-Fiction">Non-Fiction</option>
-                  <option value="Philosophical Fiction">
-                    Philosophical Fiction
-                  </option>
-                  <option value="Psychological Fiction">
-                    Psychological Fiction
-                  </option>
-                  <option value="Gothic Fiction">Gothic Fiction</option>
-                  <option value="Gothic Romance">Gothic Romance</option>
-                  <option value="Magical Realism">Magical Realism</option>
-                  <option value="Epic Poetry">Epic Poetry</option>
-                  <option value="Classic Epic">Classic Epic</option>
-                  <option value="Tech Thriller">Tech Thriller</option>
-                  <option value="Drama">Drama</option>
-                  <option value="Coming-of-Age Fiction">
-                    Coming-of-Age Fiction
-                  </option>
-                  <option value="Southern Gothic">Southern Gothic</option>
-                  <option value="Modernist Fiction">Modernist Fiction</option>
-                  <option value="Dystopian Fiction">Dystopian Fiction</option>
-                </select>
-              </div>
+                            {/* genre category */}
+                            <div className="grid grid-cols-2 gap-4">
+                              <div>
+                                <label className="block">Genre</label>
+                                <select
+                                  name="category"
+                                  className="select w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  defaultValue={edit?.category}
+                                  required
+                                >
+                                  <option value="1" disabled>
+                                    -- Select Genre --
+                                  </option>
+                                  <option value="Classic Fiction">
+                                    Classic Fiction
+                                  </option>
+                                  <option value="Historical Fiction">
+                                    Historical Fiction
+                                  </option>
+                                  <option value="Fantasy">Fantasy</option>
+                                  <option value="Science Fiction">
+                                    Science Fiction
+                                  </option>
+                                  <option value="Mystery">Mystery</option>
+                                  <option value="Thriller">Thriller</option>
+                                  <option value="Romance">Romance</option>
+                                  <option value="Adventure">Adventure</option>
+                                  <option value="Non-Fiction">
+                                    Non-Fiction
+                                  </option>
+                                  <option value="Philosophical Fiction">
+                                    Philosophical Fiction
+                                  </option>
+                                  <option value="Psychological Fiction">
+                                    Psychological Fiction
+                                  </option>
+                                  <option value="Gothic Fiction">
+                                    Gothic Fiction
+                                  </option>
+                                  <option value="Gothic Romance">
+                                    Gothic Romance
+                                  </option>
+                                  <option value="Magical Realism">
+                                    Magical Realism
+                                  </option>
+                                  <option value="Epic Poetry">
+                                    Epic Poetry
+                                  </option>
+                                  <option value="Classic Epic">
+                                    Classic Epic
+                                  </option>
+                                  <option value="Tech Thriller">
+                                    Tech Thriller
+                                  </option>
+                                  <option value="Drama">Drama</option>
+                                  <option value="Coming-of-Age Fiction">
+                                    Coming-of-Age Fiction
+                                  </option>
+                                  <option value="Southern Gothic">
+                                    Southern Gothic
+                                  </option>
+                                  <option value="Modernist Fiction">
+                                    Modernist Fiction
+                                  </option>
+                                  <option value="Dystopian Fiction">
+                                    Dystopian Fiction
+                                  </option>
+                                </select>
+                              </div>
 
-              {/* rating */}
-              <div>
-                <label htmlFor="">Rating</label>
-                <input
-                  type="number"
-                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  name="rating"
-                  min='1'
-                  max='5'
-                  required
-                />
-              </div>
-            </div>
+                              {/* rating */}
+                              <div>
+                                <label htmlFor="">Rating</label>
+                                <input
+                                  type="number"
+                                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  name="rating"
+                                  min="1"
+                                  max="5"
+                                  defaultValue={edit?.rating}
+                                  required
+                                />
+                              </div>
+                            </div>
 
-            {/* summary */}
-            <div>
-              <label className="label font-medium">Summary</label>
-              <textarea
-                name="summary"
-                required
-                rows="3"
-                className="textarea w-full rounded-2xl focus:border-0 focus:outline-gray-200 h-[250px]"
-                placeholder="Enter summary"
-                defaultValue={book.summary}
-              ></textarea>
-            </div>
+                            {/* summary */}
+                            <div>
+                              <label className="label font-medium">
+                                Summary
+                              </label>
+                              <textarea
+                                name="summary"
+                                required
+                                rows="3"
+                                className="textarea w-full rounded-2xl focus:border-0 focus:outline-gray-200 h-[250px]"
+                                placeholder="Enter summary"
+                                defaultValue={edit?.summary}
+                              ></textarea>
+                            </div>
 
-            {/* cover image */}
-            <div>
-              <label className="label font-medium">Book Cover Image</label>
-              <input
-                type="url"
-                className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                name="bookImage"
-              />
-            </div>
+                            {/* cover image */}
+                            <div>
+                              <label className="label font-medium">
+                                Book Cover Image
+                              </label>
+                              <input
+                                type="url"
+                                className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                name="bookImage"
+                                defaultValue={edit?.bookImage}
+                              />
+                            </div>
 
-            <div className="grid grid-cols-2 gap-4">
-              {/* user Email*/}
-              <div>
-                <label className="label font-medium">User Email</label>
-                <input
-                  type="email;"
-                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  name="email"
-                  defaultValue={user?.email}
-                  readOnly
-                />
-              </div>
-              {/* user name */}
-              <div>
-                <label className="label font-medium">User Name</label>
-                <input
-                  type="text"
-                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
-                  name="name"
-                  defaultValue={user?.displayName}
-                  readOnly
-                />
-              </div>
-            </div>
+                            <div className="grid grid-cols-2 gap-4">
+                              {/* user Email*/}
+                              <div>
+                                <label className="label font-medium">
+                                  User Email
+                                </label>
+                                <input
+                                  type="email;"
+                                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  name="email"
+                                  defaultValue={user?.email}
+                                  readOnly
+                                />
+                              </div>
+                              {/* user name */}
+                              <div>
+                                <label className="label font-medium">
+                                  User Name
+                                </label>
+                                <input
+                                  type="text"
+                                  className="input w-full rounded-lg focus:border-0 focus:outline-gray-200"
+                                  name="name"
+                                  defaultValue={user?.displayName}
+                                  readOnly
+                                />
+                              </div>
+                            </div>
 
-            <button
-              type="submit"
-              className="btn w-full bg-amber-700 hover:bg-amber-800 border-none text-white"
-            >
-              Add Book
-            </button>
-          </form>
+                            <button
+                              type="submit"
+                              className="btn w-full bg-amber-700 hover:bg-amber-800 border-none text-white"
+                            >
+                              Add Book
+                            </button>
+                          </form>
                           <div className="modal-action">
                             <form method="dialog">
                               {/* if there is a button in form, it will close the modal */}
